196a92bb388a71bbb2ca7746e94332a832c2264f
Kyle Becker <gnefkow87@gmail.com>
2020-03-05 22:13:09 -0500

    Added seed data, routing, modela dn controller. Everything isn't working yet, but all of the pieces should be there

---
+++ scripts/seedDB.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the ducktickerUser collection and inserts the users below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/ducktickerUsers"  //TODO: was originally "reactreadinglist" - this database needs to be created!
+);
+
+const userSeed = [
+  {
+    userName: "SamanthaSeed",
+    date: new Date(Date.now()),
+    password: "12345",
+    portfolio: [
+      "AMZN",
+      "FB",
+      "MSFT",
+      "GOOGL",
+      "F"
+    ]
+  },
+  {
+    userName: "HonoraSeed",
+    date: new Date(Date.now()),
+    password: "12345",
+    portfolio: [
+      "DIS",
+      "SNAP",
+      "UBER",
+      "NFLX",
+      "FB"
+    ]
+  },
+  {
+    userName: "KyleSeed",
+    date: new Date(Date.now()),
+    password: "12345",
+    portfolio: [
+      "TSLA",
+      "SBUX",
+      "WORK",
+      "NTDOY",
+      "TCEHY"
+    ]
+  }
+];
+
+db.User
+  .remove({})
+  .then(() => db.ducktickerUsers.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
