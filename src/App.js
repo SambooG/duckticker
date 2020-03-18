0e8cf3019945071ebafb430feb70d0615ee9606b
Samantha Gaiser <sgaiser3@gmail.com>
2020-03-17 21:25:33 -0400

    logic for holding stockapi info for user object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import SignUp from './sign-up';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import stockApi from './api/stockapi';
 import './App.css';
 
-function App() {
+const Database = [
+  {
+    email: 'john@doe.com',
+    name: "John Doe",
+    password: '3298pji32d08923p9be',
+    portfolio: ["A", "AA", "SQ"]
+  },
+  {
+    email: 'jane@doe.com',
+    name: "Jane Doe",
+    password: '3290wjoi2q;',
+    portfolio: ["AAP", "AAON", "SNAP", "SQ"]
+  }
+]
+function App() {  
+  const [data, storeData] = useState('');
+
+  const getStockData = (symbol) => {
+    stockApi(symbol, (stockData) => storeData(JSON.stringify(stockData)));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,10 +39,18 @@ function App() {
         >
           Learn React
         </a>
+        {
+          Database[1].portfolio.map(symbol => (
+            <button onClick={() => getStockData(symbol)}>
+              Get {symbol} Stock!
+            </button>
+          ))
+        }
+        {data}
       </header>
-      <SignUp />
     </div>
   );
 }
 
 export default App;
+
